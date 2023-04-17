c7e3404cb618157677b67d462950da95e5d4d858
Anson-Sun <576585666@qq.com>
2023-04-17 19:06:04 -0400

    City.java

---
+++ src/main/java/City.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+	/*
+	Create City in the tsp
+	*/
+
+public class City {
+	private int id;
+	private String crime;
+	private double x;
+	private double y;
+	/*
+	Line from a tsp file and converts it into a city object
+	*/	
+	public City(String line)
+	{
+		Scanner s = new Scanner(line);
+		this.id = s.nextInt();
+		this.x = s.nextDouble();
+		this.y = s.nextDouble();
+	}
+	public City(String crime,int id,double x,double y)
+	{
+		this.crime = crime;
+		this.id = id;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	public double getX()
+	{
+		return this.x;
+	}
+	
+	public double getY()
+	{
+		return this.y;
+	}
+
+	public String  getCrime()
+	{
+		return this.crime;
+	}
+	@Override
+    public boolean equals(Object o) {
+
+        if (o == this) return true;
+        if (!(o instanceof City))
+            return false;
+        City c = (City)o;
+        return (id == c.id);
+    }
+
+}
